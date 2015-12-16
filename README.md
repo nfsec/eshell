382250b63fc4f07edf37c3c68319d378ec260b43
nfsec <agresor@nfsec.pl>
2015-12-16 21:07:05 +0100

    Elasticsearch Shell v0.3.6

---
+++ README.md
@@ -0,0 +1,4 @@
+## Elasticsearch Interactive Shell ##
+
+EShell is a Python script that allows you to interact with a running [elasticsearch](https://www.elastic.co/products/elasticsearch) cluster
+using the APIs over HTTP.
---
+++ setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+readme = open('README.md').read()
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+setup(
+    name = 'eshell',
+    version = '0.3.6',
+    description = 'Elasticsearch interactive shell',
+    long_description = readme,
+    author = 'Patryk Krawaczyński',
+    author_email = 'agresor@nfsec.pl',
+    url = 'https://bitbucket.org/agresor/elasticshell',
+    license = 'Apache License (2.0)',
+    keywords = 'elasticsearch shell',
+    install_requires = required,
+    zip_safe = False,
+    scripts = ['eshell'],
+    classifiers = (
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: Apache Software License',
+        'Natural Language :: English',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: System :: Shells'
+    )
+)
